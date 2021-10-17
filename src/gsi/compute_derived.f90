da029cdf8e5fb75275ed4ded2cec50accffe2901
john.derber <John.Derber@noaa.gov>
2021-10-17 16:16:45 +0000

    Fixes

--- src/gsi/compute_derived.f90
+++ src/gsi/compute_derived.f90
@@ -182,6 +182,31 @@ subroutine compute_derived(mype,init_pass)
   nrf3_q=getindex(cvars3d,'q')
   iq_loc=getindex(nrf_var,'q')
 
+! Compute qsat regardless of presence of q in guess
+  iderivative=0
+  ice=.true.
+  do ii=1,nfldsig
+     call genqsat(ges_qsat(1,1,1,ii),ges_tsen(1,1,1,ii),ges_prsl(1,1,1,ii),lat2,lon2, &
+                  nsig,ice,iderivative)
+  enddo
+
+! If q in guess, check/fix q limits
+  do ii=1,nfldsig
+     call gsi_bundlegetpointer (gsi_metguess_bundle(ii),'q',ges_q,ier)
+     if (ier/=0) exit
+     if(ii == ntguessig) call q_diag(ii,mype)
+     do k=1,nsig
+        do j=1,lon2
+           do i=1,lat2
+! Limit q to be >= qmin
+              ges_q(i,j,k)=max(ges_q(i,j,k),qmin)
+! limit q to be <= ges_qsat
+              if(clip_supersaturation) ges_q(i,j,k) = min(ges_q(i,j,k),ges_qsat(i,j,k,ii))
+           end do
+        end do
+     end do
+  end do
+ 
 ! Load guess cw for use in inner loop
 ! Get pointer to cloud water mixing ratio
   it=ntguessig
